1f32bd7b3de51ced1ce678eb2f84ec377720e4a3
dnjsdbqls18 <126652295+dnjsdbqls18@users.noreply.github.com>
2024-01-03 20:09:50 +0900

    Add files via upload

---
+++ C/test_c/test_c.cpp
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(int argc, char** argv)
+{
+	printf("test1\n");
+	printf("test2\n");
+	printf("Yu Bin WON\n");
+    return 1;
+}
+
